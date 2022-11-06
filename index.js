f783f16404eca3f36217880206ef328f9e300eec
step111-lang <114953596+step111-lang@users.noreply.github.com>
2022-11-06 19:59:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ for(let i=0;i<pp.length;i++){
           clearTimeout(timer);
       }
       localStorage.setItem('tex',JSON.stringify(inp.value));
-      function butto(){
+      function butto(e){
          if(inp.value !=""){
          
           e.preventDefault()
